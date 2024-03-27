d0a615a3e6411fb353c5f7a0092c08f32f934642
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-03-27 15:25:18 -0300

    CSS module

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+import './styles.css'
 
 export function App() {
   return (
     <>
+    <Header />
       <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum sit..."
