c7c7f56c1c76a34ab4138751729dec02bcf855ee
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-03-28 18:52:48 -0300

    feat: estilizando a sidebar]

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './SideBar.module.css'
 
 export function SideBar() {
@@ -8,12 +9,16 @@ export function SideBar() {
         src="https://images.unsplash.com/photo-1581472723648-909f4851d4ae?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/40799862?v=4' />
         <strong>Hyago Mesquita</strong>
         <span>Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
