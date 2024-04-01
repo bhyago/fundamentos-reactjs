caefb38729c6ad24d84efd6630468a01b12e2f55
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-01 16:40:32 -0300

    feat: adicionar component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/40799862?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de maio às 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+          </header>
+          <button title="Deletar comentário">
+            <Trash size={20} />
+          </button>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
