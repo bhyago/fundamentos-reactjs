3e80512860e849fd213dbe96386d6d401e23a36f
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-02 15:42:55 -0300

    feat: adicionar component avatar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './SideBar.module.css'
+import { Avatar } from './Avatar'
 
 export function SideBar() {
   return (
@@ -9,7 +10,7 @@ export function SideBar() {
         src="https://images.unsplash.com/photo-1581472723648-909f4851d4ae?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/40799862?v=4' />
+        <Avatar src="https://avatars.githubusercontent.com/u/40799862?v=4"/>
         <strong>Hyago Mesquita</strong>
         <span>Developer</span>
       </div>
