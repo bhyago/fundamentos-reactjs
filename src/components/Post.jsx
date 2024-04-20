31ca5322e02b1bd96b2cd009cbf7a80645e09898
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-20 20:01:21 -0300

    feat: adicionar comentarios com programação declarativa

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,14 +11,22 @@ import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1, 2
+    'Post muito bacana, hein!'
   ])
+
+  const [newCommentText, setNewCommentText] = useState('')
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
   const publisherDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: true })
 
   function handleCreateNewComment() {
     event.preventDefault()
-    setComments([...comments, comments.length + 1])
+
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
 
  return (
@@ -46,7 +54,12 @@ export function Post({ author, publishedAt, content }) {
 
     <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
       <strong>Deixe seu feedback</strong>
-      <textarea placeholder='Deixe um comentário'/>
+      <textarea 
+        name="comment" 
+        placeholder='Deixe um comentário'
+        value={newCommentText}
+        onChange={handleNewCommentChange}
+        />
       <footer>
         <button type="submit">Publicar</button>
       </footer>
@@ -54,7 +67,7 @@ export function Post({ author, publishedAt, content }) {
 
     <div className={styles.commentList}>
       {comments.map(comment => {
-        return <Comment />
+        return <Comment  content={comment} />
       })}
 
     </div>
