d5b83fc15d46910daf5d05ca59b301f33d2ff62d
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-20 21:33:51 -0300

    feat: validar formulario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import { format, formatDistanceToNow  } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
 
@@ -26,7 +26,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -37,53 +42,56 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeleteOne)
   }
 
- return (
-  <article className={styles.post}>
-    <header>
-      <div className={styles.author}>
-        <Avatar src={author.avatarUrl}/>
-        <div className={styles.authorInfo}>
-          <strong>{author.name}</strong>
-          <span>{author.role}</span>
+  const isNewCommentEmpty = newCommentText.length === 0
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
         </div>
+
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publisherDateRelativeToNow}</time>
+      </header>
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href='#'>{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publisherDateRelativeToNow}</time>
-    </header>
-    <div className={styles.content}>
-      {content.map(line => {
-        if (line.type === 'paragraph') {
-          return <p key={line.content}>{line.content}</p>
-        } else if (line.type === 'link') {
-          return <p key={line.content}><a href='#'>{line.content}</a></p>
-        }
-      })}
-    </div>
-
-    <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
-      <strong>Deixe seu feedback</strong>
-      <textarea 
-        name="comment" 
-        placeholder='Deixe um comentário'
-        value={newCommentText}
-        onChange={handleNewCommentChange}
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          name="comment"
+          placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
-      <footer>
-        <button type="submit">Publicar</button>
-      </footer>
-    </form>
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
+        </footer>
+      </form>
 
-    <div className={styles.commentList}>
-      {comments.map(comment => {
-        return (
-        <Comment 
-          key={comment} 
-          content={comment} 
-          onDeleteComment={deleteComment}
-        />)
-      })}
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />)
+        })}
 
-    </div>
-  </article>
- ) 
+      </div>
+    </article>
+  )
 }
