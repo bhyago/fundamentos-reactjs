3cc78805660e963bcfe12a70f3a37ab6ad94d71f
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-20 21:44:23 -0300

    feat: adicionar likes aos comentarios

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,13 +2,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
   }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false}  src="https://avatars.githubusercontent.com/u/40799862?v=4"/>
@@ -27,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
