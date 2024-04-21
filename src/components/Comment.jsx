82f68fc3b00528fa6b835403be033cf02363c6cf
Hyago Mesquita <hyago.mesquita@spotmetrics.com>
2024-04-20 21:07:02 -0300

    feat: adicionar comunicação entre componets

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({ content }) {
+
+
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false}  src="https://avatars.githubusercontent.com/u/40799862?v=4"/>
@@ -14,7 +19,7 @@ export function Comment({ content }) {
               <strong>Diego Fernandes</strong>
               <time title="11 de maio às 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
